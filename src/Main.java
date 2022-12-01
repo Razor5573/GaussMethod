a6b4e76c64082465ef64ea0e794104a4733799ea
Никита Калугин <nikitakalugin02@gmail.com>
2022-12-01 21:36:20 +0700

    fixed

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double[][] augmented_matrix = new double[3][4];
+        double[][] a = new double[3][4];
+        double[][] temp = new double[3][4];
+        double[][] temp2 = new double[3][4];
+
         Scanner in = new Scanner(System.in);
 
         for (int i = 0; i < 3; i++) {
@@ -14,51 +17,57 @@ public class Main {
                     System.out.println("Print b_" + i + " coefficient");
                 }
 
-                augmented_matrix[i][j] = in.nextDouble();
+                a[i][j] = in.nextDouble();
+                temp[i][j] = a[i][j];
+                temp2[i][j] = a[i][j];
             }
         }
+        Counter.transpose(temp2);
+        temp2 =  Counter.getAlgebraicAdditionsMatrix(temp2);
+
+        double[][] reversed = Counter.getReversed(temp2, Counter.getDeterminant(a));
+        double conditionNumber = Counter.getConditionNumber(a, reversed);
 
         for (int i = 0; i < 3; i++) {
-            if (augmented_matrix[i][i] == 0) {
+            if (temp[i][i] == 0) {
                 for(int j = i + 1; j < 3; j++){
-                    if(augmented_matrix[j][i] != 0.0){
+                    if(temp[j][i] != 0.0){
                         for (int k = 0; k < 4; k++){
-                            double tmp = augmented_matrix[j][k];
-                            augmented_matrix[j][k] = augmented_matrix[i][k];
-                            augmented_matrix[i][k] = tmp;
+                            double tmp = temp[j][k];
+                            temp[j][k] = temp[i][k];
+                            temp[i][k] = tmp;
                         }
                     }
                 }
             }
             for (int j = i; j < 3; j++){
-                double temp_coef = augmented_matrix[j][i];
+                double temp_coef = temp[j][i];
                 if(temp_coef == 0.0){
-                   /*for(int k = i; k < 3; k++){
-                       double tmp = augmented_matrix[k][j];
-                       augmented_matrix[k][j] = augmented_matrix[k + 1][j];
-                       augmented_matrix[j + 1][k] = tmp;
-                   }*/
                     continue;
                 }
                 for (int k = i; k < 4; k++) {
-                    augmented_matrix[j][k] = augmented_matrix[j][k] / temp_coef;
+                    temp[j][k] = temp[j][k] / temp_coef;
                 }
             }
             for (int j = i; j < 2; j++){
                 for (int k = i; k < 4; k++) {
-                    if(augmented_matrix[j + 1][k] == 0.0){
+                    if(temp[j + 1][k] == 0.0){
                         break;
                     }
-                    augmented_matrix[j + 1][k] = augmented_matrix[j + 1][k] - augmented_matrix[i][k];
+                    temp[j + 1][k] = temp[j + 1][k] - temp[i][k];
                 }
             }
         }
-        double x3 = augmented_matrix[2][3] / augmented_matrix[2][2];
-        double x2 = augmented_matrix[1][3] - augmented_matrix[1][2] * x3;
-        double x1 = augmented_matrix[0][3] - augmented_matrix[0][2] * x3 - augmented_matrix[0][1] * x2;
+        double x3 = temp[2][3] / temp[2][2];
+        double x2 = temp[1][3] - temp[1][2] * x3;
+        double x1 = temp[0][3] - temp[0][2] * x3 - temp[0][1] * x2;
+        double q = Counter.getNorm(a);
 
-        System.out.println("x1 =" + x1);
-        System.out.println("x2 =" + x2);
-        System.out.println("x3 =" + x3);
+
+        System.out.println("x1 = " + x1);
+        System.out.println("x2 = " + x2);
+        System.out.println("x3 = " + x3);
+        System.out.println("Matrix norm = " + q);
+        System.out.println("Matrix condition number = " + conditionNumber);
     }
 }
